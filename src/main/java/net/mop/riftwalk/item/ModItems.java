39534683c2ba6792b01620f0efbb7df47678ae19
Jacob <jacobpurdue88@gmail.com>
2025-06-20 02:17:50 +1200

    Defined helper methods for item registration and initialization. Added a template item to test functionality.

---
+++ src/main/java/net/mop/riftwalk/item/ModItems.java
@@ -0,0 +1,29 @@
+package net.mop.riftwalk.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.mop.riftwalk.Riftwalk;
+
+public class ModItems {
+    public static final Item RIFTSHARD = registerItem("riftshard", new Item(new FabricItemSettings()));
+
+    private static void addItemsToIngredientTabItemGroup(FabricItemGroupEntries entries) {
+        entries.add(RIFTSHARD);
+    }
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(Riftwalk.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        Riftwalk.LOGGER.info("Registering Mod Items for " + Riftwalk.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientTabItemGroup);
+    }
+}
