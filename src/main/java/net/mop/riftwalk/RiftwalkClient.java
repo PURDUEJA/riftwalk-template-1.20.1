71e19d77fe1d1ec87ac7e28d1597c34dde13afaf
Jacob <jacobpurdue88@gmail.com>
2025-06-20 01:08:27 +1200

    Initial commit

---
+++ src/main/java/net/mop/riftwalk/RiftwalkClient.java
@@ -0,0 +1,10 @@
+package net.mop.riftwalk;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RiftwalkClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
